49b21782ba18b432514dcf3e8358e413a1c6197b
Thaís <thais_de_souza@hotmail.com>
2020-09-29 21:57:49 -0300

    Terminando de inserir metodos de soma, subtração, divisão e multiplicação

--- Calculadora/src/operacao/Calculo.java
+++ Calculadora/src/operacao/Calculo.java
@@ -5,5 +5,20 @@ public class Calculo {
 	public int soma(int num1, int num2) {
 		return num1 + num2;
 	}
+	
+	public int sub (int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public int div(int num1,int num2){
+	    
+		return num1 / num2;
+		
+	}
+	public int mult(int num1, int num2){
+	     
+		return num1 - num2;
+		
+	}
 
 }
